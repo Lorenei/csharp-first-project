0ffc76ca3fe074496a3701d46b2b3537ae5f9d9d
Arkadiusz Richert <arshenik@gmail.com>
2017-08-06 14:18:26 +0200

    Cleanup, comments in MainWindow.xaml.cs.

--- MainChatWindow/MainChatWindow/MainWindow.xaml.cs
+++ MainChatWindow/MainChatWindow/MainWindow.xaml.cs
@@ -18,14 +18,10 @@ using System.Reflection;
 using System.Windows.Threading;
 
 namespace ChatClient {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
     
     public partial class MainWindow : Window {
 
-        //Service vars
+        //Service variables
         public static IChatService Server;
         private static DuplexChannelFactory<IChatService> _channelFactory;
         private ClientCallback _clientCallback;
@@ -46,44 +42,40 @@ namespace ChatClient {
         private bool AmIBeingKicked = false;
 
         public MainWindow(string userName, string userPassword, string userRoomName, bool RegisterAsNewUser = false) {
-            InitializeComponent(); //If it's possible it could be moved as last command so we won't need to use close if initialization fails?
-            //Closing += new CancelEventHandler(LogoutFromServer);
+            InitializeComponent(); 
 
+            //Fill the combo box with colors.
             ColorPickerComboBox.ItemsSource = typeof(Colors).GetProperties();
 
             usersList = new Dictionary<string, string>();
             InitializeVariables(userName, userPassword, userRoomName);
             InitializeClientCallback();
+            //Try to connect to server.
             if(!InitializeServerConnection(RegisterAsNewUser))
             {
                 MessageBox.Show("Failed to login. Application shutdown.");
                 Application.Current.Shutdown();
             }
-            //temp number for debug multi clients
-            /*int tmpLogin = 0;
-            _LOGIN_ += tmpLogin;
-            while(!InitializeServerConnection()) {
-                MessageBox.Show("Login failed, trying again.");
-                tmpLogin++;
-                _LOGIN_ = "Lorenei" + tmpLogin;
-            }*/
+
             RefreshUsersList();
             InitializeLogFile();
             StatusTextBlock_1.Text = "Welcome " + userName + " to chat room! Have a nice day! ";
             StatusBarClock();
         }
+        /// <summary>
+        /// Destroyer method - called just before class is destroyed.
+        /// Try to close connection with server when closing chat window.
+        /// </summary>
         ~MainWindow() {
             if (!AmIBeingKicked && _IS_CONNECTION_DONE_) {
                 CloseConnectionWithServer();
             }
         }
+        /// <summary>
+        /// Abort connection with server.
+        /// Should be trying to close it and if it fails aborting. But whatever.
+        /// </summary>
         private void CloseConnectionWithServer() {
-            /*try {
-                _channelFactory.Close();
-            }
-            catch(Exception e) {
-                _channelFactory.Abort();
-            }*/
             _channelFactory.Abort();
         }
         //Send logout request to server before closing app window.
@@ -100,24 +92,27 @@ namespace ChatClient {
             }
             _channelFactory.Abort();
         }
-        //Add single user to users list and refresh the list
-        //Need some refactoring, since I don't think that replacing resources with whole new list is proper.
-        //Need to find solution how to make automatically sorted resources that allow to add one item to it.
+        /// <summary>
+        /// Add single new user to users list.
+        /// </summary>
         public void AddUserToList(string userName, Color userColor)
         {
             usersList.Add(userName, userColor.ToString());
             Resources["UsersList"] = usersList.OrderBy(user => user.Key);
             AddMessageToFlowDocument(userName + " joined chat room.");
         }
-        //Remove single user from users list and refresh the list.
-        //Need the same refactoring as AddUserToList().
+        /// <summary>
+        /// Remove single user from users list.
+        /// </summary>
         public void RemoveUserFromList(string userName)
         {
             usersList.Remove(userName);
             Resources["UsersList"] = usersList.OrderBy(user => user.Key);
             AddMessageToFlowDocument(userName + " left chat room.");
         }
-        //Refresh whole users list with list delivered in parameter or get new one from server.
+        /// <summary>
+        /// Refresh whole users list.
+        /// </summary>
         public void RefreshUsersList(Dictionary<string, string> _usersList = null) {
             
             if (_usersList != null)
@@ -130,18 +125,28 @@ namespace ChatClient {
             }
             Resources["UsersList"] = usersList.OrderBy(user => user.Key);
         }
+        /// <summary>
+        /// Set basic variables that were taken from login form.
+        /// </summary>
         public void InitializeVariables(string userName, string userPassword, string userRoomName) {
             _LOGIN_ = userName;
             _PASSWORD_ = userPassword;
             _ROOM_NAME_ = userRoomName;
         }
+        /// <summary>
+        /// Create callback channel.
+        /// </summary>
         public void InitializeClientCallback() {
             _clientCallback = new ClientCallback();
         }
+        /// <summary>
+        /// Create connection with service.
+        /// </summary>
         public bool InitializeServerConnection(bool RegisterAsNewUser) {
             _channelFactory = new DuplexChannelFactory<IChatService>(new ClientCallback(), "ChatServiceEndPoint");
             Server = _channelFactory.CreateChannel();
             
+            //If user wants to be registered, negiotiate with server.
             if(RegisterAsNewUser)
             {
                 if(!Server.RegisterNewUserToDB(_LOGIN_, _PASSWORD_)) {
@@ -155,36 +160,45 @@ namespace ChatClient {
                 return false;
             }
             else {
+                //Add LogoutFromServer event only after login is successful. Else application would
+                //try to disconnect from nonexistent server.
                 Closing += new CancelEventHandler(LogoutFromServer);
                 _IS_CONNECTION_DONE_ = true;
                 return true;
             }
         }
+        /// <summary>
+        /// Create chat logs.
+        /// </summary>
         public void InitializeLogFile() {
             chatLog = new ChatLogAPI();
             GenerateLogFile();
         }
+        /// <summary>
+        /// Receiver of "Enter" key press when focusing message box.
+        /// </summary>
         private void UserMessageTextBox_OnKeyDownHandler(object sender, KeyEventArgs e) {
             if(e.Key == Key.Return) {
                 string tempString = UserMessageTextBox.Text;
                 if(tempString != null && tempString != "") {
-                    //AddMessageToFlowDocument(tempString);
                     try
                     {
-                        Server.SendMessageToAll(tempString, _LOGIN_);
+                        Server.SendMessageToAll(tempString, _LOGIN_); //Send message to server (and other clients)
                     }
                     catch(Exception err)
                     {
                         AddMessageToFlowDocument("Failed to send message to server. Error e: " + err.ToString());
                     }
-                    AddMessageToFlowDocument(tempString, _LOGIN_);
-                    UserMessageTextBox.Clear();
-                    //MessageBox.Show("Message sent"); //Debug function
+                    AddMessageToFlowDocument(tempString, _LOGIN_); //Add your own message directly to chat window.
+                    UserMessageTextBox.Clear(); //Clear message text box.
                 }
             }
         }
 
-        //should be activated by server response about whether message was accepted or not
+        /// <summary>
+        /// Method that adds text to chat window. It also generates lines for chat log.
+        /// Can also handle commands texts.
+        /// </summary>
         public void AddMessageToFlowDocument(string message, string userName = "") {
             if(message == "" || message == null) {
                 return;
@@ -199,24 +213,26 @@ namespace ChatClient {
             }
             //Adding loadedBlock event to event listener so when new message is rendered it will bring it to view (scroll to new message in chat)
             //It does work, but probably should be added just once at initialization or something..
+            //This event is needed since BringToView function right now would try to bring to view elements that are not yet completely rendered.
             tempParagraph.Loaded += loadedBlock;
             OknoChatowe.Document.Blocks.Add(tempParagraph);
 
-            //write our message to our log file
+            //Write our message to our log file
             SaveToLog(_ROOM_NAME_, _message_to_log);
-
-            //tempParagraph.BringIntoView();
         }
-        //method added to event to scroll main chat window to last added message
+        /// <summary>
+        /// Method added to event to scroll main chat window to last added message 
+        /// </summary>
         private void loadedBlock(object sender, RoutedEventArgs e) {
             var bloc = sender as Block;
             if(bloc != null) {
                 bloc.BringIntoView();
             }
         }
-
-        //method to layout message before adding it to the main chat window
-        //_message_ is a string that goes out of this method and returns constructed, stylised (in future :P) html message that will be written in log file.
+        /// <summary>
+        /// Method to layout message before adding it to the main chat window
+        /// _message_ is a string that goes out of this method and returns constructed, stylised (in future :P) html message that will be written in log file. 
+        /// </summary>
         private Paragraph layoutMessage(string _nick, string _message, out string _message_, bool isThisCommand = false) {
             Paragraph tempP = new Paragraph();
             var _datetime_ = DateTime.Now;
@@ -242,51 +258,75 @@ namespace ChatClient {
 
             return tempP;
         }
-
+        /// <summary>
+        /// Method that layouts our message for log file.
+        /// </summary>
         private string layoutForLogMessage(string time, string nick, string msg) {
             string layoutedMessage = "[" + time + "] " + nick + ": " + msg + "<br />" + Environment.NewLine;
 
             return layoutedMessage;
         }
 
+        /// <summary>
+        /// Method that creates new chat log.
+        /// </summary>
         private void GenerateLogFile() {
             if(!chatLog.CreateNewChatLog(_ROOM_NAME_)) {
                 Application.Current.Shutdown();
             }
         }
 
+        /// <summary>
+        /// Method that saves message to log file.
+        /// </summary>
         private void SaveToLog(string roomName, string _message_to_log) {
             if(!chatLog.WriteToChatLog(roomName, _message_to_log)) {
                 Application.Current.Shutdown();
             }
         }
 
+        /// <summary>
+        /// Method that responds to IP button.
+        /// </summary>
         private void ShowIPButton_Click(object sender, RoutedEventArgs e) {
             if(UsersListBox.SelectedItem == null) {
                 AddMessageToFlowDocument("Command Run -> Show IP of user: Failed! You have to select user from list.");
             }
             else {
                 //Access to users list userName
-                //MessageBox.Show(((KeyValuePair<string, int>) UsersListBox.SelectedItem).Key);
                 string selectedUser = ((KeyValuePair<string, string>)UsersListBox.SelectedItem).Key;
-                string ipAddress = Server.ShowIpInfo(_LOGIN_, selectedUser);
-                string message = "Command Run -> Show IP of user: " + selectedUser + " -> IP : " + ipAddress;
-                AddMessageToFlowDocument(message);
+                try
+                {
+                    string ipAddress = Server.ShowIpInfo(_LOGIN_, selectedUser);
+                    string message = "Command Run -> Show IP of user: " + selectedUser + " -> IP : " + ipAddress;
+                    AddMessageToFlowDocument(message);
+                }
+                catch(Exception err)
+                {
+                    AddMessageToFlowDocument("Command Run -> Show IP of user: failed!");
+                }
             }
         }
-
+        /// <summary>
+        /// Method that responds to user being kicked.
+        /// </summary>
         public void YouHaveBeenKicked(string userName) {
             string message = "You have been kicked from server by: " + userName;
             AddMessageToFlowDocument(message);
             AmIBeingKicked = true;
             this.Close();
         }
+        /// <summary>
+        /// Method that responds to user being banned.
+        /// </summary>
         public void YouHaveBeenBanned(string userName) {
             AddMessageToFlowDocument("You have been banned from server by: " + userName);
             AmIBeingKicked = true;
             this.Close();
         }
-
+        /// <summary>
+        /// Method that responds to Kick button
+        /// </summary>
         private void KickUserButton_Click(object sender, RoutedEventArgs e) {
             if (UsersListBox.SelectedItem == null) {
                 AddMessageToFlowDocument("Command Run -> Kick user: Failed! You have to select user from list.");
@@ -298,9 +338,7 @@ namespace ChatClient {
                 string selectedUser = ((KeyValuePair<string, string>)UsersListBox.SelectedItem).Key;
                 string message = "Command Run -> Kick user: " + selectedUser + ". Request have been sent to server.";
                 AddMessageToFlowDocument(message);
-                //This is temporary solution. It should be changed to properly kick someone.
-                //But since I have no idea how to handle true two-way connection with server, I have to make sure
-                //that server doesn't try to send 'remove user from list' function on the same channel.
+
                 bool tempKick = false;
                 try
                 {
@@ -311,12 +349,13 @@ namespace ChatClient {
                     AddMessageToFlowDocument("Failed to contact server about kicking user. Error e: " + err.ToString());
                 }
                 if(tempKick) {
-                    //MessageBox.Show(_LOGIN_);
                     RemoveUserFromList(selectedUser);
                 }
             }
         }
-
+        /// <summary>
+        /// Method that responds to Ban button.
+        /// </summary>
         private void BanUserButton_Click(object sender, RoutedEventArgs e) {
             if (UsersListBox.SelectedItem == null) {
                 AddMessageToFlowDocument("Command Run -> Ban User: Failed! You have to select user from list.");
@@ -332,12 +371,13 @@ namespace ChatClient {
                 }
             }
         }
-
+        /// <summary>
+        /// Method that responds to color being picked.
+        /// </summary>
         private void OnColorPick(object sender, SelectionChangedEventArgs e)
         {
             Color selectedColor = (Color)(ColorPickerComboBox.SelectedItem as PropertyInfo).GetValue(null, null);
             UpdateColorForUser(_LOGIN_, selectedColor);
-            AddMessageToFlowDocument("OnColorPick(object, selectionchangedeventargs): Zmieniono swój kolor na: " + selectedColor.ToString());
             try
             {
                 Server.ChangeUserColor(_LOGIN_, selectedColor);
@@ -346,9 +386,11 @@ namespace ChatClient {
             {
                 AddMessageToFlowDocument("Failed to contact server about changing color. Error e: " + err.ToString());
             }
-            AddMessageToFlowDocument("OnColorPick(object, selectionchangedeventargs): WYsłano informację do serwera o zmianie koloru użytkownika.");
+            AddMessageToFlowDocument("Server has been contacted about your color change.");
         }
-
+        /// <summary>
+        /// Method that responds to other users color change.
+        /// </summary>
         public void GetUserColor(string userName, Color userColor)
         {
             if (usersList.ContainsKey(userName))
@@ -357,25 +399,33 @@ namespace ChatClient {
                 UpdateColorForUser(userName, userColor);
             }
         }
-
+        /// <summary>
+        /// Method that updates color for users.
+        /// </summary>
         private void UpdateColorForUser(string userName, Color userColor)
         {
             usersList[userName] = userColor.ToString();
-            AddMessageToFlowDocument("UpdateColorForUser(string,color): Podmieniono kolor użytkownika: " + userName + " na: " + userColor.ToString());
             Resources["UsersList"] = usersList.OrderBy(user => user.Key);
-            AddMessageToFlowDocument("UpdateColorForUser(string,color): Odświeżono resources.");
         }
-
+        /// <summary>
+        /// Method that responds to menu bar item About.
+        /// </summary>
         private void About_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Arkadiusz Richert\narshenik@gmail.com\ngithub.com/Lorenei\n2017 - current.");
         }
+        /// <summary>
+        /// Method that responds to menu bar item Exit.
+        /// </summary>
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             LogoutFromServer(new object(), new CancelEventArgs());
             CloseConnectionWithServer();
             Application.Current.Shutdown();
         }
+        /// <summary>
+        /// Method that makes clock to show on status bar.
+        /// </summary>
         private void StatusBarClock()
         {
             DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
