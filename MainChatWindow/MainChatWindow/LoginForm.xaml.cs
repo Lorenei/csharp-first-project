6db7a11338c7f7813dbdbd794e5135b978917625
Arkadiusz Richert <arshenik@gmail.com>
2017-08-06 13:09:40 +0200

    Cleanup and comments to login form. Many things aren't needed in c-sharp-academy project, since they were added for future reference.

--- MainChatWindow/MainChatWindow/LoginForm.xaml.cs
+++ MainChatWindow/MainChatWindow/LoginForm.xaml.cs
@@ -13,58 +13,31 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace ChatClient {
-    /// <summary>
-    /// Interaction logic for LoginForm.xaml
-    /// </summary>
+
     public partial class LoginForm : Window {
 
         MainWindow mainWindow;
 
         public LoginForm() {
             InitializeComponent();
-            //mainWindow = new MainWindow();
-            //this.Owner = chatWindow;
         }
 
         private void SignInButton_Click(object sender, RoutedEventArgs e) {
             //Disable all input boxes so user can't change anything while program is trying to establish connection with server.
             DisableInputBoxes();
 
+            //Safety check.
             if(!ValidateInput())
             {
                 MessageBox.Show("You must enter your username and password to proceed.");
                 EnableInputBoxes();
                 return;
             }
-            if(RegisterUserCheckBox.IsChecked == true)
-            {
-                //Will have to add server connection to loginform so we can register, and try to login here instead of already in chat window.
-                /*if(!RegisterNewUser())
-                {
-                    MessageBox.Show("Failed to register new user. Please try different user name.");
-                    EnableInputBoxes();
-                    return;
-                }
-                else
-                {
-                    MessageBox.Show("Registration completed. You will be loged in now.");
-                }*/
-            }
 
             mainWindow = new MainWindow(LoginTextBox.Text, PasswordTextBox.Password, RoomTextBox.Text, RegisterUserCheckBox.IsChecked.Value);
-            //mainWindow.InitializeVariables(LoginTextBox.Text, PasswordTextBox.Password, RoomTextBox.Text);
-            //if (!mainWindow._IS_CONNECTION_DONE_)
-            //{
-            //MessageBox.Show("Failed to login!");
-            //EnableInputBoxes();
-            //return;
-            //}
 
-            //mainWindow.InitializeLogFile();
-            //mainWindow.InitializeClientCallback();
-            //mainWindow.RefreshUsersList();
-            this.Hide();
-            mainWindow.Show();
+            this.Hide(); //Hide login form.
+            mainWindow.Show(); //Show main chat window.
             try
             {
                 this.Owner = mainWindow;
@@ -75,16 +48,25 @@ namespace ChatClient {
             }
         }
 
+        /// <summary>
+        /// Enabled all input boxes.
+        /// </summary>
         private void EnableInputBoxes() {
             LoginTextBox.IsEnabled = true;
             PasswordTextBox.IsEnabled = true;
             RoomTextBox.IsEnabled = true;
         }
+        /// <summary>
+        /// Disables all input boxes.
+        /// </summary>
         private void DisableInputBoxes() {
             LoginTextBox.IsEnabled = false;
             PasswordTextBox.IsEnabled = false;
             RoomTextBox.IsEnabled = false;
         }
+        /// <summary>
+        /// Validate input boxes.
+        /// </summary>
         private bool ValidateInput()
         {
             if(PasswordTextBox.Password == "" || PasswordTextBox.Password == null || LoginTextBox.Text == "" || LoginTextBox.Text == null)
@@ -95,12 +77,6 @@ namespace ChatClient {
             {
                 return true;
             }
-        }
-
-        private bool RegisterNewUser()
-        {
-            
-            return false;
         }
     }
 }
